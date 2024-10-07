03d5fe97d2e1f723dfa5e8e50c12d84c74fcc8da
Mustapha Boudouch <mustaphaboudouch@gmail.com>
2024-10-07 17:45:04 +0200

    style: style password forms

--- lib/screens/manager/user_edit_screen.dart
+++ lib/screens/manager/user_edit_screen.dart
@@ -5,7 +5,10 @@ import 'package:kermanager/api/api_response.dart';
 import 'package:kermanager/providers/auth_provider.dart';
 import 'package:kermanager/router/auth/routes.dart';
 import 'package:kermanager/services/user_service.dart';
-import 'package:kermanager/widgets/password_input.dart';
+import 'package:kermanager/theme/theme_size.dart';
+import 'package:kermanager/widgets/button.dart';
+import 'package:kermanager/widgets/form_column.dart';
+import 'package:kermanager/widgets/password_form_input.dart';
 import 'package:kermanager/widgets/screen.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -23,55 +26,66 @@ class UserEditScreen extends StatefulWidget {
 }
 
 class _UserEditScreenState extends State<UserEditScreen> {
+  final _formKey = GlobalKey<FormState>();
+
   final UserService _userService = UserService();
 
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _newPasswordController = TextEditingController();
 
   Future<void> _submit() async {
-    ApiResponse<Null> response = await _userService.edit(
-      userId: widget.userId,
-      password: _passwordController.text,
-      newPassword: _newPasswordController.text,
-    );
-    if (response.error != null) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(response.errorMessage),
-        ),
+    if (_formKey.currentState!.validate()) {
+      ApiResponse<Null> response = await _userService.edit(
+        userId: widget.userId,
+        password: _passwordController.text,
+        newPassword: _newPasswordController.text,
       );
-    } else {
-      SharedPreferences preferences = await SharedPreferences.getInstance();
-      await preferences.remove(ApiConstants.tokenKey);
-      Provider.of<AuthProvider>(context, listen: false)
-          .setUser(-1, "", "", "", false);
-      context.go(AuthRoutes.signIn);
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('Password edited successfully'),
-        ),
-      );
+      if (response.error != null) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(response.errorMessage),
+          ),
+        );
+      } else {
+        SharedPreferences preferences = await SharedPreferences.getInstance();
+        await preferences.remove(ApiConstants.tokenKey);
+        Provider.of<AuthProvider>(context, listen: false)
+            .setUser(-1, "", "", "", false);
+        context.go(AuthRoutes.signIn);
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Password edited successfully'),
+          ),
+        );
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Screen(
+      appBar: AppBar(
+        title: const Text('Modifier le mot de passe'),
+      ),
       children: [
-        const Text(
-          "Password Edit",
-        ),
-        PasswordInput(
-          hintText: "Password",
-          controller: _passwordController,
-        ),
-        PasswordInput(
-          hintText: "New password",
-          controller: _newPasswordController,
-        ),
-        ElevatedButton(
-          onPressed: _submit,
-          child: const Text('Save'),
+        FormColumn(
+          formKey: _formKey,
+          children: [
+            PasswordFormInput(
+              hintText: "Mot de passe",
+              controller: _passwordController,
+            ),
+            const SizedBox(height: ThemeSize.s16),
+            PasswordFormInput(
+              hintText: "Nouveau mot de passe",
+              controller: _newPasswordController,
+            ),
+            const SizedBox(height: ThemeSize.s16),
+            Button(
+              onTap: _submit,
+              label: "Modifier",
+            ),
+          ],
         ),
       ],
     );
